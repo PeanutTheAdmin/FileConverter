7dcf3d19e7ed6611f061a7662d3089bd068644bc
PeanutTheAdmin <cavanessjacob@gmail.com>
2020-12-04 21:57:00 -0600

    Getting Started

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+from wand.image import Image as wi
+pdf = wi(filename="sample.pdf", resolution=300)
+pdfimage = pdf.convert("jpeg")
+i=1
+for img in pdfimage.sequence:
+    page = wi(image=img)
+    page.save(filename=str(i)+".jpg")
+    i +=1
