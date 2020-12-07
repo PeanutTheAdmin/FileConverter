8860f2e180aa5501de948d31938fac45574dd245
PeanutTheAdmin <68164840+PeanutTheAdmin@users.noreply.github.com>
2020-12-07 14:41:48 -0600

    Added output statements for different functions

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import argparse
 
 def get_arguments(): # gets arguments and files from user
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", dest="file", help="File to be converted, (e.g. sample.pdf, apple.png, baseball.jpg)")
-    parser.add_argument("-ct" , "--convert_type", dest="convert_type", help="New file type to be converted to, (e.g. png or jpg)")
+    parser.add_argument("-f", "--file", dest="file", help="File to be converted, (e.g. sample.pdf, apple.png, baseball.jpg).")
+    parser.add_argument("-ct" , "--convert_type", dest="convert_type", help="New file type to be converted to, (e.g. png or jpg).")
     (options) = parser.parse_args()
     if not options.file:
         parser.error("[-] Please specify a file, use --help for more info.")
@@ -16,28 +16,37 @@ def get_arguments(): # gets arguments and files from user
         parser.error("[-] Please specify a file type to be converted to, use --help for more info.")
     return options
 
-def check_file(options): # selects settings based on user choice
+def check_file(options): # checks if file, and file type to be converted exsits
+    print(f"[+] Checking if {options.file} exsits.")
     if path.exists(options.file) == True:
+        print(f"[+] File {options.file} found.")
         file_name = options.file[:-4]
-        extension_list = {"png": "png", "jpg": "jpeg"}
-        file_extension = extension_list[options.convert_type]
-        if file_extension == "png" or "jpeg":
+        # if options.convert_type == "png" or "jpg":
+        try:
+            extension_list = {"png": "png", "jpg": "jpeg"}
+            file_extension = extension_list[options.convert_type]
             return file_name, file_extension
-        else:
-            print("File type to be converted to was not found or not supported. Please check the extension before trying again, (e.g. png or jpg)")
+        except KeyError:
+            print(f"[-] File type {options.convert_type} to be converted to was not found. Please check the extension before trying again, (e.g. png or jpg).")
             exit()
     else: 
-        print("File specified was not found. Please check the name and extension before trying again. (e.g. sample.pdf)")
+        print(f"[-] File {options.file} not found. Please check the name and extension before trying again. (e.g. sample.pdf).")
         exit()
 
 def convert_file(options, file_name, file_extension): # converts files to other formats
-    pdf = wi(filename=options.file, resolution=300)
-    pdfimage = pdf.convert(file_extension)
-    i=1
-    for img in pdfimage.sequence:
-        page = wi(image=img)
-        page.save(filename=f"{file_name}{str(i)}.{options.convert_type}")
-        i +=1
+    try:
+        print(f"[+] Converting {options.file} to {file_name}.{options.convert_type}")
+        pdf = wi(filename=options.file, resolution=300)
+        pdfimage = pdf.convert(file_extension)
+        i=1
+        for img in pdfimage.sequence:
+            page = wi(image=img)
+            page.save(filename=f"{file_name}{str(i)}.{options.convert_type}")
+            i +=1
+        print("[+] File was successfully converted.")
+    except:
+        print("[-] File was not successfully converted.")
+        exit()
 
 def main(): # Main Function
     options = get_arguments()
